f653a665c14a7f0a5efbe7f8c9f3e1840be70a89
Alexander <sashok935@gmail.com>
2018-04-18 19:29:12 +0300

    Add base functions

---
+++ FTPFileExplorer/FtpClient.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace FTPFileExplorer
+{
+    class FtpClient
+    {
+        public class Client
+        {
+            private string username;
+            private string password = "";
+            private string url;
+            private int bufSize = 1024;
+
+            public bool Passive = true;
+            public bool Binary = true;
+            public bool EnableSSL = false;
+            public bool Hash = false;
+
+            public Client(string url, string username, string password)
+            {
+                this.url = url;
+                this.username = username;
+                this.password = password;
+            }
+
+            private string CombinePaths(string path1, string path2)
+            {
+                return Path.Combine(path1, path2).Replace("\\", "/");
+            }
+
+            private FtpWebRequest CreateRequest(string method)
+            {
+                return CreateRequest(url, method);
+            }
+
+            private FtpWebRequest CreateRequest(string url, string method)
+            {
+                var r = (FtpWebRequest)WebRequest.Create(url);
+
+                r.Credentials = new NetworkCredential(username, password);
+                r.Method = method;
+                r.UseBinary = Binary;
+                r.EnableSsl = EnableSSL;
+                r.UsePassive = Passive;
+
+                return r;
+            }
+
+            private string GetStatusDescription(FtpWebRequest request)
+            {
+                using (var response = (FtpWebResponse)request.GetResponse())
+                {
+                    return response.StatusDescription;
+                }
+            }
+
+            public string PrintDir()
+            {
+                var req = CreateRequest(WebRequestMethods.Ftp.PrintWorkingDirectory);
+
+                return GetStatusDescription(req);
+            }
+
+            public string ChangeDir(string path)
+            {
+                url = CombinePaths(url, path);
+                return PrintDir();
+            }
+
+            public string[] ListDirectory()
+            {
+                List<string> list = new List<string>();
+
+                FtpWebRequest request = CreateRequest(WebRequestMethods.Ftp.ListDirectory);
+
+                using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+                {
+                    using (Stream stream = response.GetResponseStream())
+                    {
+                        using (StreamReader reader = new StreamReader(stream, true))
+                        {
+                            while (!reader.EndOfStream)
+                            {
+                                list.Add(reader.ReadLine());
+                            }
+                        }
+                    }
+                }
+
+                return list.ToArray();
+            }
+
+            public string[] ListDirectoryDetails()
+            {
+                List<string> list = new List<string>();
+
+                FtpWebRequest request = CreateRequest(WebRequestMethods.Ftp.ListDirectoryDetails);
+
+                using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+                {
+                    using (Stream stream = response.GetResponseStream())
+                    {
+                        using (StreamReader reader = new StreamReader(stream, true))
+                        {
+                            while (!reader.EndOfStream)
+                            {
+                                list.Add(reader.ReadLine());
+                            }
+                        }
+                    }
+                }
+
+                return list.ToArray();
+            }
+
+
+        }
+    }
+}
--- FTPFileExplorer/MainWindow.xaml.cs
+++ FTPFileExplorer/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace FTPFileExplorer
 {
@@ -20,9 +23,116 @@ namespace FTPFileExplorer
     /// </summary>
     public partial class MainWindow : Window
     {
+        string prevAddress = "ftp://";
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void ConnectBtnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                FtpClient.Client client = new FtpClient.Client(addressBox.Text, loginBox.Text, passBox.Password);
+
+                Regex regex = new Regex(@"^([d-])([rwxt-]{3}){3}\s+\d{1,}\s+.*?(\d{1,})\s+(\w+\s+\d{1,2}\s+(?:\d{4})?)(\d{1,2}:\d{2})?\s+(.+?)\s?$",
+                    RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
+
+                List<FileDirectoryInfo> list = client.ListDirectoryDetails()
+                    .Select(s =>
+                    {
+                        Match match = regex.Match(s);
+                        if (match.Length > 5)
+                        {
+                            string type = match.Groups[1].Value == "d" ? "dir" : "file";
+                            string img = match.Groups[1].Value == "d" ? "folder.png" : "file.png";
+                            string size = "";
+                            if (type == "File.png")
+                                size = (Int64.Parse(match.Groups[3].Value.Trim()) / 1024).ToString() + "kBytes";
+
+                            return new FileDirectoryInfo(size, type, match.Groups[6].Value, match.Groups[4].Value, img, addressBox.Text);
+                        }
+                        else
+                            return new FileDirectoryInfo();
+                    }).ToList();
+                list.Add(new FileDirectoryInfo("", "up", "...", "", "up.png", addressBox.Text));
+                list.Reverse();
+
+                lbx_files.DataContext = list;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString() + ": \n" + ex.Message);
+            }
+        }
+
+        private void FolderClick(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount >= 2)
+            {
+                FileDirectoryInfo fdi = (FileDirectoryInfo)(sender as StackPanel).DataContext;
+                if (fdi.Type == "dir")
+                {
+                    prevAddress = fdi.address;
+                    addressBox.Text = fdi.address + fdi.Name + "/";
+                    ConnectBtnClick(null, null);
+                }
+                // else if (click on up) { return }
+            }
+        }
+    }
+
+    public class FileDirectoryInfo
+    {
+        string fileSize;
+        string type;
+        string name;
+        string date;
+        string img;
+        public string address;
+
+        public string FileSize
+        {
+            get { return fileSize; }
+            set { fileSize = value; }
+        }
+
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public String Img
+        {
+            get { return img; }
+            set { img = value; }
+        }
+
+        public FileDirectoryInfo() { }
+
+        public FileDirectoryInfo(string fileSize, string type, string name, string date, string img, string address)
+        {
+            FileSize = fileSize;
+            Type = type;
+            Name = name;
+            Date = date;
+            Img = img;
+            this.address = address;
+        }
+
     }
 }
